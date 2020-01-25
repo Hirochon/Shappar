22d5f7ada97ebea882fab296e6b86c190b60f62f
Hirochon <heacet43@gmail.com>
2020-01-25 16:18:58 +0900

    [del] 余計なファイルを消去

--- config/settings/local_s.py
+++
@@ -1,8 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j#n6welzh!n5l-i6@s)yc0r!a3zddg0apb+7_#l!e$j-i%()u6'
-
-##################
-# Authentication #
-##################
-
-AUTH_USER_MODEL = 'accounts.CustomUser'
