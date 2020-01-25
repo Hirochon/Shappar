cf4e328547d37e70a2b81144b7a1214ee8b94365
Hirochon <heacet43@gmail.com>
2020-01-25 20:46:36 +0900

    [add] 認証のフォームを定義

---
+++ accounts/forms.py
@@ -0,0 +1,25 @@
+from allauth.account.forms import SignupForm
+from django import forms
+from .models import CustomUser
+
+class MyCustomSignupForm(SignupForm):
+    username = forms.CharField(label='ID', required=True, help_text='※必須',
+                                widget=forms.TextInput(attrs={'placeholder':'taro1225'}))
+    usernonamae = forms.CharField(label='ユーザ名', required=True, help_text='※必須',
+                                widget=forms.TextInput(attrs={'placeholder':'太郎'}))
+    email = forms.EmailField(label='メールアドレス', required=True, help_text='※必須',
+                                widget=forms.TextInput(attrs={'placeholder':'taro@example.com'}))
+    sex = forms.ChoiceField(label='性別', required=True, choices=CustomUser.choice_site)
+    age = forms.IntegerField(label='年齢', required=True, help_text='※必須', min_value=0, max_value=150)
+    born_at = forms.DateTimeField(label='生年月日', required=False)
+
+    def save(self, request):
+
+        # Ensure you call the parent class's save.
+        # .save() returns a User object.
+        user = super(MyCustomSignupForm, self).save(request)
+
+        # Add your own processing here.
+
+        # You must return the original result.
+        return user
