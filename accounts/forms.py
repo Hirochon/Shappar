aeef96967cb77fda9cab7eb09662aab65d408c97
Hirochon <heacet43@gmail.com>
2020-01-25 21:15:35 +0900

    [mod] 生年月日の見本を追加

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,7 +11,7 @@ class MyCustomSignupForm(SignupForm):
                                 widget=forms.TextInput(attrs={'placeholder':'taro@example.com'}))
     sex = forms.ChoiceField(label='性別', required=True, choices=CustomUser.choice_site)
     age = forms.IntegerField(label='年齢', required=True, help_text='※必須', min_value=0, max_value=150)
-    born_at = forms.DateTimeField(label='生年月日', required=False)
+    born_at = forms.DateTimeField(label='生年月日', required=False, widget=forms.TextInput(attrs={'placeholder':'1998-08-10'}))
 
     def save(self, request):
 
