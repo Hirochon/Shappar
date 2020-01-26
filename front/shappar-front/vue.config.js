442d2ad2bed2436721790cdbdc39be4fa66b4d18
fujiya228 <fujiya0xE2@gmail.com>
2020-01-27 02:30:46 +0900

    [mod]devServerのtargetをバックに合わせた

--- front/shappar-front/vue.config.js
+++ front/shappar-front/vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
     devServer: {
       proxy: {
         "/api/": {
-          target: "http://localhost:3000/",
+          target: "http://localhost:8000/",
         }
       }
     }
