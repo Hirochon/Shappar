b5b06ee4df7c0bb7ba5a8e0e85108a9980dffcad
Hirochon <heacet43@gmail.com>
2020-01-26 12:00:03 +0900

    [mod] forms.pyのコードを綺麗に

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,14 +3,11 @@ from django import forms
 from .models import CustomUser
 
 class MyCustomSignupForm(SignupForm):
-    username = forms.CharField(label='ID', required=True, help_text='※必須',
-                                widget=forms.TextInput(attrs={'placeholder':'taro1225'}))
-    usernonamae = forms.CharField(label='ユーザ名', required=True, help_text='※必須',
-                                widget=forms.TextInput(attrs={'placeholder':'太郎'}))
-    email = forms.EmailField(label='メールアドレス', required=True, help_text='※必須',
-                                widget=forms.TextInput(attrs={'placeholder':'taro@example.com'}))
+    username = forms.CharField(label='ID', required=True, widget=forms.TextInput(attrs={'placeholder':'taro1225'}))
+    usernonamae = forms.CharField(label='ユーザ名', required=True, widget=forms.TextInput(attrs={'placeholder':'太郎'}))
+    email = forms.EmailField(label='メールアドレス', required=True, widget=forms.TextInput(attrs={'placeholder':'taro@example.com'}))
     sex = forms.ChoiceField(label='性別', required=True, choices=CustomUser.choice_site)
-    age = forms.IntegerField(label='年齢', required=True, help_text='※必須', min_value=0, max_value=150)
+    age = forms.IntegerField(label='年齢', required=True, min_value=0, max_value=150)
     born_at = forms.DateField(label='生年月日', required=False, widget=forms.SelectDateWidget(years=range(1900,2021)))
 
     def save(self, request):
