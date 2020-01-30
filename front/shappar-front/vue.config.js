a41ed1fb9fa08d2e45b15eb699bd57af74059cd8
fujiya228 <fujiya0xE2@gmail.com>
2020-01-30 23:41:13 +0900

    [mod]vue.config.jsのシンタックスエラーを修正

--- front/shappar-front/vue.config.js
+++ front/shappar-front/vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    outputDir: '../../static',
-    indexPath: '../templates/index.html',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/static/'
-      : '/',
-    devServer: {
-      proxy: {
-        '/api/': {
-          target: 'http://localhost:8000/',
-        }
+  outputDir: '../../static',
+  indexPath: '../templates/index.html',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/static/'
+    : '/',
+  devServer: {
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:8000/'
       }
     }
   }
+}
