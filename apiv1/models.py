dd370b5673185820581185eafe2951962c6bc3b9
Hirochon <heacet43@gmail.com>
2020-02-01 00:39:31 +0900

    [add] mediaファイルをgitignore

--- apiv1/models.py
+++ apiv1/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth import get_user_model
 
 def get_favorite_image_path(instance, filename):
-    return 'images/fav/{0}/{1}'.format(instance.submitter.id, filename)
+    return 'images/mypage/{0}/{1}'.format(instance.submitter.id, filename)
 
 class Mypage(models.Model):
     """マイページ"""
@@ -12,6 +13,7 @@ class Mypage(models.Model):
 
     user = models.ForeignKey(get_user_model(), verbose_name='ユーザ', on_delete=models.CASCADE, related_name='mypage_user')
     intro = models.CharField(verbose_name='自己紹介', max_length=150, blank=True, null=True)
+    #submitter = models.ForeignKey(settings.AUTH_USER_MODEL, models.PROTECT, verbose_name='投稿者')
     homeimage = models.ImageField(verbose_name='ホーム画像', blank=True, null=True, upload_to=get_favorite_image_path)
 
     def __str__(self):
