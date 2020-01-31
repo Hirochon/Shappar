7d2ef6d5a3b244bf5440115cb707e34d3cdeece2
Hirochon <heacet43@gmail.com>
2020-02-01 00:21:59 +0900

    [add] mypageのモデルを追加

--- apiv1/models.py
+++ apiv1/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+def get_favorite_image_path(instance, filename):
+    return 'images/fav/{0}/{1}'.format(instance.submitter.id, filename)
+
+class Mypage(models.Model):
+    """マイページ"""
+
+    class Meta:
+        db_table = 'mypage'
+
+    user = models.ForeignKey(get_user_model(), verbose_name='ユーザ', on_delete=models.CASCADE, related_name='mypage_user')
+    intro = models.CharField(verbose_name='自己紹介', max_length=150, blank=True, null=True)
+    homeimage = models.ImageField(verbose_name='ホーム画像', blank=True, null=True, upload_to=get_favorite_image_path)
+
+    def __str__(self):
+        return self.user.usernonamae
