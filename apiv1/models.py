8c2797169143f2f6ea19fc1b49fecee3334b5757
Hirochon <heacet43@gmail.com>
2020-02-01 19:56:13 +0900

    [add] マイページのモデルを作成

--- apiv1/models.py
+++ apiv1/models.py
@@ -2,19 +2,18 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-def get_favorite_image_path(instance, filename):
-    return 'images/mypage/{0}/{1}'.format(instance.submitter.id, filename)
+def get_homeimage_path(instance, filename):
+    return 'images/{0}/homeimage/{1}'.format(instance.user.id, filename)
 
 class Mypage(models.Model):
     """マイページ"""
 
     class Meta:
         db_table = 'mypage'
-
+    
     user = models.ForeignKey(get_user_model(), verbose_name='ユーザ', on_delete=models.CASCADE, related_name='mypage_user')
-    intro = models.CharField(verbose_name='自己紹介', max_length=150, blank=True, null=True)
-    #submitter = models.ForeignKey(settings.AUTH_USER_MODEL, models.PROTECT, verbose_name='投稿者')
-    homeimage = models.ImageField(verbose_name='ホーム画像', blank=True, null=True, upload_to=get_favorite_image_path)
-
+    introduction = models.CharField(verbose_name='自己紹介', max_length=150, blank=True, null=True)
+    homeimage = models.ImageField(verbose_name='ホーム画像', blank=True, null=True, upload_to=get_homeimage_path)
+    
     def __str__(self):
         return self.user.usernonamae
