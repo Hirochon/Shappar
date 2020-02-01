b931d0de422b5a62991be4f18849ef17e88600a7
Hirochon <heacet43@gmail.com>
2020-02-01 21:39:45 +0900

    [add] DEBUG=True時のメディアファイル保存先を追加

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api/v1/', include('apiv1.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
