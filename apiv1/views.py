339406f3fed38894b3240511b3a006189602e8f8
Hirochon <heacet43@gmail.com>
2020-02-01 19:57:00 +0900

    [add] マイページのviewsを実装

--- apiv1/views.py
+++ apiv1/views.py
@@ -1,12 +1,23 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.shortcuts import get_object_or_404
+from rest_framework import views, status
+from rest_framework.response import Response
 
 from django.contrib.auth import get_user_model
-from .serializers import UserSerializer
+from .models import Mypage
+from .serializers import MypageSerializer
 
-class UserViewSet(viewsets.ModelViewSet):
-    """本モデルのCRUD用APIクラス"""
+class MypageAPIView(views.APIView):
+    """マイページ用APIクラス"""
 
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    def get(self, request, pk, *args, **kwargs):
+        """マイページモデルをGETメソッド"""
+
+        """エンドポイントをユーザIDによってマイページを取得する場合"""
+        user = get_user_model().objects.get(username=pk)
+        mypage = get_object_or_404(Mypage, user_id=user.id)
+
+        # """エンドポイントをUUIDによってマイページを取得する場合"""
+        # mypage = get_object_or_404(Mypage, user_id=pk)
+
+        serializer = MypageSerializer(instance=mypage)
+        return Response(serializer.data, status.HTTP_200_OK)
