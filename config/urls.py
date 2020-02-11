daa6a1bbe51d3d3f7df46e1f78a07e0f3ecf71f7
Hirochon <heacet43@gmail.com>
2020-02-12 02:19:28 +0900

    [add] JWT認証のエンドポイントを追加

--- config/urls.py
+++ config/urls.py
@@ -5,11 +5,18 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('accounts/', include('accounts.urls')),
-    path('api/v1/', include('apiv1.urls')),
     path('test/', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('accounts/', include('accounts.urls')),
+
+    """APIのエンドポイントを設定"""
+    path('api/v1/', include('apiv1.urls')),
+
+    """JWT認証のエンドポイントを設定"""
+    path('api/v1/auth/', include('djoser.urls.jwt')),
+
 ]
 
 if settings.DEBUG:
