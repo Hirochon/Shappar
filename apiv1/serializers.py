cf89282113d560dafeab2db283feb19d5b3bfdc4
Hirochon <heacet43@gmail.com>
2020-02-13 17:19:26 +0900

    [add] マイページ用シリアライザを変更

--- apiv1/serializers.py
+++ apiv1/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import Mypage
+from .models import Mypage, Post
 
 class MypageSerializer(serializers.ModelSerializer):
     """マイページ用シリアライザ"""
 
-    user_id = serializers.ReadOnlyField(source='user.id')
-    username = serializers.CharField(source='user.username')
-    usernonamae = serializers.CharField(source='user.usernonamae')
+    unique_id = serializers.ReadOnlyField(source='user.id')
+    user_id = serializers.CharField(source='user.username')
+    name = serializers.CharField(source='user.usernonamae')
     iconimage = serializers.ImageField(source='user.iconimage')
 
     class Meta:
         model = Mypage
-        fields = ['user_id','username','usernonamae','introduction','iconimage','homeimage']
+        fields = ['unique_id','user_id','name','introduction','iconimage','homeimage']
+
+class PostSerializer(serializer.ModelSerializer):
+    """投稿用のシリアライザ"""
+
+    user_id = serializers.ReadOnlyField(source='user.id')
+
+    class Meta:
+        model = Post
+        fields = ['id','user_id']
