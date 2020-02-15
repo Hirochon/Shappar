253722ba16960348c7182597ea6de39ef81c7b7c
Hirochon <heacet43@gmail.com>
2020-02-16 02:46:07 +0900

    [add] 投票についてルーティングの設定を追加

--- apiv1/urls.py
+++ apiv1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-from .views import MypageAPIView
+from .views import MypageAPIView, PollListCreateAPIView
 
 app_name = 'apiv1'
 urlpatterns = [
     path('users/<pk>/', MypageAPIView.as_view()),
+    path('polls/', PollListCreateAPIView.as_view()),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.urls')),
 ]
