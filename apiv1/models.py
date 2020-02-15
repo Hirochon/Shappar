04422356d953852804aa17c181083a6998da53f3
Hirochon <heacet43@gmail.com>
2020-02-16 02:44:46 +0900

    [add] 投票モデルを再構成

--- apiv1/models.py
+++ apiv1/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.contrib.auth import get_user_model
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator,MaxValueValidator,MinValueValidator 
 
 
 class Post(models.Model):
@@ -22,7 +22,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(default=timezone.now, editable=False)
 
     def __str__(self):
-        return self.question
+        return '投稿者: ' + self.user.usernonamae + '(' + self.user.username + ') 質問: ' + self.question
 
 
 class Poll(models.Model):
@@ -31,14 +31,15 @@ class Poll(models.Model):
     class Meta:
         db_table = 'poll'
 
-    post = models.OneToOneField(Post, verbose_name='投稿', on_delete=models.PROTECT, related_name='poll_post')
+    post = models.ForeignKey(Post, verbose_name='投稿', on_delete=models.PROTECT, related_name='poll_post')
     user = models.ForeignKey(get_user_model(), verbose_name='投票者', on_delete=models.PROTECT, related_name='poll_user')
     voted = models.BooleanField(verbose_name='投稿したかどうか', default=False)
     total = models.IntegerField(verbose_name='合計投票数', default=0)
-    num_1 = models.IntegerField(verbose_name='小計1', default=0)
-    num_2 = models.IntegerField(verbose_name='小計2', default=0)
-    num_3 = models.IntegerField(verbose_name='小計3', default=0)
-    num_4 = models.IntegerField(verbose_name='小計4', default=0)
+    select = models.IntegerField(verbose_name='投票先', blank=False, null=False, validators=[MinValueValidator(1),MaxValueValidator(4)])
+    num_1 = models.IntegerField(verbose_name='小計1', default=0, validators=[MinValueValidator(0)])
+    num_2 = models.IntegerField(verbose_name='小計2', default=0, validators=[MinValueValidator(0)])
+    num_3 = models.IntegerField(verbose_name='小計3', default=0, validators=[MinValueValidator(0)])
+    num_4 = models.IntegerField(verbose_name='小計4', default=0, validators=[MinValueValidator(0)])
 
     def __str__(self):
-        return self.user.usernonamae + "(" + self.user.username + ")" + self.post.question + "(" + str(self.total) + ")"
+        return self.user.usernonamae + "(" + self.user.username + ") 質問:" + self.post.question + " (投票先:" + str(self.select) + ")"
