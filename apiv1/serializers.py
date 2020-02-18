54073f02d55094d6d59b625c639d04de1c5be4e5
Hirochon <heacet43@gmail.com>
2020-02-19 01:57:57 +0900

    [add] Optionのシリアライザを追加

--- apiv1/serializers.py
+++ apiv1/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import Post, Poll
+from .models import Post, Poll, Option
 
 class MypageSerializer(serializers.ModelSerializer):
     """マイページ用シリアライザ"""
@@ -14,15 +14,24 @@ class MypageSerializer(serializers.ModelSerializer):
         fields = ['unique_id', 'user_id', 'name', 'introduction', 'iconimage', 'homeimage']
 
 
+class OptionSerializer(serializers.ModelSerializer):
+    """選択肢用シリアライザ"""
+
+    class Meta:
+        model = Option
+        fields = ['select_num', 'answer', 'share_id']
+
+
 class PostCreateSerializer(serializers.ModelSerializer):
-    """投稿用のシリアライザ"""
+    """投稿用シリアライザ"""
 
     class Meta:
         model = Post
-        fields = ['user', 'question', 'answer_1', 'answer_2', 'answer_3', 'answer_4']
+        fields = ['user', 'question','share_id']
 
-class PostRetrieveSerializer(serializers.ModelSerializer):
-    """投稿一覧取得のシリアライザ"""
+
+class PostListSerializer(serializers.ModelSerializer):
+    """投稿一覧取得シリアライザ"""
 
     post_id = serializers.ReadOnlyField(source='id')
     user_id = serializers.ReadOnlyField(source='user.username')
@@ -35,13 +44,12 @@ class PostRetrieveSerializer(serializers.ModelSerializer):
 
 
 class PollSerializer(serializers.ModelSerializer):
-    """投票用のシリアライザ"""
+    """投票用シリアライザ"""
 
     post_id = serializers.ReadOnlyField(source='post.id')
-    unique_id = serializers.ReadOnlyField(source='user.id')
     user_id = serializers.ReadOnlyField(source='user.username')
     iconimage = serializers.ImageField(source='user.iconimage')
 
     class Meta:
         model = Poll
-        fields = ['id','post_id', 'unique_id', 'user_id', 'iconimage', 'voted', 'total', 'select', 'num_1', 'num_2', 'num_3', 'num_4']
+        fields = ['id','post_id', 'unique_id', 'user_id', 'iconimage', 'voted']
