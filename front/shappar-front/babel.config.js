af53e3d7a25237a6ada3061c44a9f06d8d1e8289
fujiya228m <fujiya0xE2@gmail.com>
2020-02-19 22:44:21 +0900

    [mod].babelrcを削除し、babel.config.jsに書き換え

--- front/shappar-front/babel.config.js
+++ front/shappar-front/babel.config.js
@@ -1,5 +1,14 @@
+// .babelrcもbabelの設定ファイルで、違いは
+//    babel.config.js:プロジェクト全体の設定
+//    .babelrc:ファイル個別の設定
+// その結果babel.config.jsより優先されておりコンパイルエラー
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  env: {
+    test: {
+      presets: [['env', { 'targets': { 'node': 'current' } }]]
+    }
+  }
 }
