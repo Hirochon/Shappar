a4811cc8adbfa9ccae17a2c5e38d6d053d270281
Hirochon <heacet43@gmail.com>
2020-02-19 12:34:56 +0900

    [add] 投票時のviewを実装

--- apiv1/views.py
+++ apiv1/views.py
@@ -4,9 +4,10 @@ from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from django_filters import rest_framework as filters
 from django.http import HttpResponseNotFound
+import uuid
 
 from django.contrib.auth import get_user_model
-from .models import Poll, Post
+from .models import Poll, Post, Option
 from .serializers import MypageSerializer, PostCreateSerializer, PostListSerializer, PollSerializer, OptionSerializer
 
 class MypageAPIView(views.APIView):
@@ -54,6 +55,11 @@ class PostCreateAPIView(views.APIView):
         if 10 < len(data['options']):
             return HttpResponseNotFound('<h1>Bad Request!</h1>')
 
+        share_uuid = uuid.uuid4()
+        data['share_id'] = share_uuid
+        for datas in data['options']:
+            datas['share_id'] = share_uuid
+
         user = get_object_or_404(get_user_model(), id=data['unique_id'])
         data['user'] = user.id
         del data['unique_id']
@@ -99,10 +105,29 @@ class PostListAPIView(views.APIView):
 class PollCreateAPIView(views.APIView):
     """投票モデルの登録APIクラス"""
 
-    def post(self, request, pk, qk, *args, **kwargs):
+    def post(self, request, pk, *args, **kwargs):
         """投票時の登録APIに対応するハンドラメソッド"""
 
-        serializer = PollSerializer(data=request.data)
+        data = request.data
+        data['post'] = pk
+        data['user'] = data['unique_id']
+        del data['unique_id']
+
+        data_option = data['option']
+        del data['option']
+        post = Post.objects.get(id=data['post'])
+        options = Option.objects.filter(share_id=post.share_id)
+        for option in options:
+            if option.select_num == data_option['select_num']:
+                option.votes += 1
+                data_option['votes'] = option.votes
+                data_option['share_id'] = option.share_id
+                data_option['id'] = option.id
+                break
+        serializer_option = OptionSerializer(instance=Option.objects.get(id=data_option['id']) ,data=data_option)
+        serializer_option.is_valid(raise_exception=True)
+        serializer_option.save()
+        serializer = PollSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
