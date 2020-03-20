7add422db64669e9eabaebe6a9fd052f733b1530
Hirochon <heacet43@gmail.com>
2020-03-20 16:01:38 +0900

    [add] テスト実行に向けて設定ファイルを変更

--- config/settings/local.py
+++ config/settings/local.py
@@ -202,6 +202,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
 
