110e56eacc42af29353ecdfdb2a6746e6764f821
Hirochon <heacet43@gmail.com>
2020-03-21 21:43:15 +0900

    [add] (正常)投票していないユーザーは投票結果閲覧不可テスト(200)

--- apiv1/tests/test_views.py
+++ apiv1/tests/test_views.py
@@ -637,6 +637,57 @@ class TestPostUpdateAPIView(APITestCase):
         }
         self.assertJSONEqual(response.content, expected_json_dict)
 
+    def test_get_update_posts_no_voted_success(self):
+        """投稿モデルの投票情報更新APIへのGETリクエスト(正常系:投票していないユーザーは投票結果閲覧不可)"""
+
+        # 投稿用のユーザーがログイン→投稿
+        token = str(RefreshToken.for_user(self.user1).access_token)
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
+        params = {
+            'question':'あなたの推しメンは？',
+            'options':[{
+                'select_num':0,
+                'answer':'齋藤飛鳥'
+            },{
+                'select_num':1,
+                'answer':'北野日奈子'
+            }]
+        }
+        self.client.post('/api/v1/posts/', params, format='json')
+        
+        # 他ユーザーにてログイン→投稿情報更新
+        token = str(RefreshToken.for_user(self.user2).access_token)
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
+        
+        post = Post.objects.get()
+        response = self.client.get(self.TARGET_URL_WITH_PK.format(post.id))
+        self.assertEqual(response.status_code, 200)
+        
+        options = Option.objects.filter(share_id=post.share_id)
+        options_list = []
+        for option in options:
+            option_dict = {}
+            option_dict['select_num'] = option.select_num
+            option_dict['answer'] = option.answer
+            option_dict['votes'] = -1
+            options_list.append(option_dict)
+        post_created_at = str(post.created_at)
+        hours = timedelta(hours=9)
+        utc_created_at = datetime.strptime(post_created_at, '%Y-%m-%d %H:%M:%S.%f%z')
+        created_at = utc_created_at + hours
+        expected_json_dict = {
+            'post_id':str(post.id),
+            'user_id':str(post.user.username),
+            'iconimage':circleci.MEDIA_URL + str(post.user.iconimage),
+            'question':post.question,
+            'created_at':created_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            'voted':False,
+            'selected_num':-1,
+            'total':0,
+            'options':options_list
+        }
+        self.assertJSONEqual(response.content, expected_json_dict)
+
 
 # (正常系)1method,(異常系)3methods,(合計)4methods.
 class TestPollCreateAPIView(APITestCase):
