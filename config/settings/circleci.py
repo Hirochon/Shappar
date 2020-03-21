82abf4abab84afb9611b8df10769a49a657f8b95
Hirochon <heacet43@gmail.com>
2020-03-21 15:11:19 +0900

    [mod] テスト環境を本番/ローカル環境に近づけるよう修正

--- config/settings/circleci.py
+++ config/settings/circleci.py
@@ -1,14 +1,18 @@
 import os
-from datetime import timedelta
 import environ
+from datetime import timedelta
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# 環境変数を設定
 env = environ.Env(DEBUG=(bool,False),ALLOWED_HOSTS=(list,[]))
-# env.read_env(os.path.join(BASE_DIR,'.env'))
+env.read_env(os.path.join(BASE_DIR,'.env'))
 
-SECRET_KEY = 'ts*b*i0^-8+m@%6151^ko6=a6o0s7om^!*#1)$^fc0fehnj*m2'
-DEBUG = False
+SECRET_KEY = env('SECRET_KEY')
+
+DEBUG = env('DEBUG')
+
 ALLOWED_HOSTS = ['*']
 
 
@@ -34,6 +38,11 @@ INSTALLED_APPS = [
     'allauth.account',                  #allauthの基本的なログイン認証系
     'allauth.socialaccount',            #ソーシャル認証
 
+    # AWS
+    'django_ses',                       #AmazonSESとの連携アプリ
+    'storages',                         #AmazonS3との連携アプリ
+    'django_cleanup',                   #必要のない静的ファイルを自動消去アプリ
+
     # 3rd party apps
     'rest_framework',                   #RESTFrameworkアプリ
     'djoser',                           #エンドポイントを設定
@@ -50,6 +59,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
+
+
+########
+# CORS #
+########
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+)
 
 
 ROOT_URLCONF = 'config.urls'
@@ -73,7 +93,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi_circleci.application'
+WSGI_APPLICATION = 'config.wsgi_local.application'
 
 
 DATABASES = {
@@ -121,24 +141,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-
-AWS_S3_CUSTOM_DOMAIN = env('AWS_S3_CUSTOM_DOMAIN')
-
-STATIC_URL = 'https://%s/static/' % AWS_S3_CUSTOM_DOMAIN
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = 'https://%s/static/' % AWS_S3_CUSTOM_DOMAIN
-
-
-# Media Files
-
-MEDIA_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
-MEDIA_ROOT = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
-
 ###########################
 # Authentication(allauth) #
 ###########################
 
+ACCOUNT_EMAIL_SUBJECT_PREFIX = "[Shappar(しゃぱー)]"
 SITE_ID = 1     #サイトの識別ID
 LOGIN_REDIRECT_URL = 'home'         #ログイン後のリダイレクト先
 LOGOUT_REDIRECT_URL = '/accounts/login/'    #ログアウト後のリダイレクト先
@@ -151,6 +158,41 @@ AUTHENTICATION_BACKENDS = (
 )
 ACCOUNT_ADAPTER = 'accounts.adapter.CustomAccountAdapter'
 
+
+#######################
+# Amazon Web Services #
+#######################
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+
+# Amazon SES settings
+
+EMAIL_BACKEND = env('EMAIL_BACKEND')
+DEFAULT_FROM_EMAIL = SERVER_EMAIL = env('DEFAULT_FROM_EMAIL')
+
+# Amazon S3 settings
+
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME        # CloudFront無しの場合
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)          # CloudFront無しの場合
+
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = env('AWS_S3_CUSTOM_DOMAIN')
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+AWS_DEFAULT_ACL = None
+
+STATIC_ROOT = 'https://%s/static/' % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = 'https://%s/static/' % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Mediaファイルの設定
+MEDIA_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
+MEDIA_ROOT = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
 
 ##################
 # REST Framework #
