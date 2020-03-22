a780f51bf917293920e6b9c1de725651183b321d
Hirochon <heacet43@gmail.com>
2020-03-22 22:51:50 +0900

    [add] (異常)OptionSerializerのjsonリクエストで項目が足りていない

--- apiv1/tests/test_serializer.py
+++ apiv1/tests/test_serializer.py
@@ -17,6 +17,17 @@ from apiv1.serializers import (
 class TestOptionSerializer(TestCase):
     """OptionSerializerのテストクラス"""
 
+    def change_required(self, input_data, field):
+        """何度も使う処理を関数化(method:test_input_invalid_something_is_required)"""
+
+        serializer = OptionSerializer(data=input_data)
+        self.assertEqual(serializer.is_valid(), False)
+        self.assertCountEqual(serializer.errors.keys(), [field])
+        self.assertCountEqual(
+            [x.code for x in serializer.errors[field]],
+            ['required'],
+        )
+
     def test_input_valid(self):
         """入力データのバリデーション(OK)"""
 
@@ -31,3 +42,24 @@ class TestOptionSerializer(TestCase):
 
         # バリデーションの結果を検証
         self.assertEqual(serializer.is_valid(), True)
+
+    def test_input_invalid_something_is_required(self):
+        """入力データのバリデーション(NG:何かがリクエストされていない)"""
+
+        input_data = {
+            'select_num': 0,
+            'answer':'テスト',
+            'votes':3,
+            'share_id':uuid.uuid4()
+        }
+        del input_data['select_num']
+        # 事前に定義した関数を実行
+        self.change_required(input_data, 'select_num')
+
+        input_data['select_num'] = 0
+        del input_data['answer']
+        self.change_required(input_data, 'answer')
+
+        input_data['answer'] = 'テスト'
+        del input_data['share_id']
+        self.change_required(input_data, 'share_id')
