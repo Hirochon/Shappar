462d76a5dd57a4823ba2c1505f3e9a69a3995752
Hirochon <heacet43@gmail.com>
2020-03-31 23:27:49 +0900

    [add] 管理者画面に血液型の項目を追加

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,11 +7,11 @@ from .models import CustomUser
 class CustomUserAdmin(UserAdmin):
     """管理画面の表示する項目を設定"""
 
-    list_display = ('id', 'usernonamae', 'username', 'email', 'age', 'sex', 'is_staff')
+    list_display = ('id', 'usernonamae', 'username', 'email', 'age', 'sex', 'blood_type', 'is_staff')
 
     fieldsets = (
         (None, {'fields':('username','password')}),
-        (_('Personal info'), {'fields': ('usernonamae', 'email', 'age', 'sex','born_at', 'introduction', 'iconimage', 'homeimage')}),
+        (_('Personal info'), {'fields': ('usernonamae', 'email', 'age', 'sex', 'blood_type', 'born_at', 'introduction', 'iconimage', 'homeimage')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
--- apiv1/admin.py
+++ apiv1/admin.py
@@ -13,17 +13,17 @@ class CustomOptionAdmin(admin.ModelAdmin):
 class CustomPostAdmin(admin.ModelAdmin):
     """Postの管理画面の表示する項目を設定"""
 
-    list_display = ('id', 'question', 'user', 'share_id')
+    list_display = ('id', 'question', 'user', 'total', 'share_id')
     readonly_fields = ('id', 'share_id','created_at')
-    fields = ('id', 'question', 'share_id','created_at')
+    fields = ('id', 'question', 'total', 'share_id','created_at')
 
 
 class CustomPollAdmin(admin.ModelAdmin):
     """Optionの管理画面の表示する項目を設定"""
 
-    list_display = ('id','user','post','option')
-    readonly_fields = ('id',)
-    fields = ('id','user','post','option')
+    list_display = ('id','post', 'user', 'option', 'created_at')
+    readonly_fields = ('id','post', 'user', 'option', 'created_at')
+    fields = ('id','post', 'user', 'option', 'created_at')
 
 admin.site.register(Option, CustomOptionAdmin)
 admin.site.register(Post, CustomPostAdmin)
