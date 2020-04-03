27ef7b2ea7f99a72bf19a5e09c46e05ad495015c
Hirochon <heacet43@gmail.com>
2020-04-03 16:30:39 +0900

    [mod] 年齢のフォームをセレクトに変更

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,14 +7,14 @@ class MyCustomSignupForm(SignupForm):
     """サインアップ時のフォーム"""
 
     # 年齢のタプルを作成
-    # def create_choices():
-    #     choices_list = []
-    #     for i in range(0, 151):
-    #         choice = (i, i)
-    #         choices_list.append(choice)
-    #     choices = tuple(choices_list)
-    #     return choices
-    # choices = create_choices()
+    def create_choices():
+        choices_list = []
+        for i in range(0, 151):
+            choice = (i, i)
+            choices_list.append(choice)
+        choices = tuple(choices_list)
+        return choices
+    choices = create_choices()
 
     # フィールドを指定
     username = forms.CharField(label='ID', required=True, widget=forms.TextInput(attrs={'placeholder': 'taro1225'}))
@@ -23,8 +23,8 @@ class MyCustomSignupForm(SignupForm):
     sex = forms.ChoiceField(label='性別', required=True, choices=CustomUser.sex_choice_site)
     blood_type = forms.ChoiceField(label='血液型', required=True, choices=CustomUser.blood_type_choice_site)
     age = forms.IntegerField(label='年齢', required=True, min_value=0, max_value=150, widget=forms.NumberInput(attrs={'placeholder': 0}))
-    # age = forms.IntegerField(label='年齢', required=True, min_value=0, max_value=150, widget=forms.Select(choices=choices))
-    born_at = forms.DateField(label='生年月日', required=True, widget=forms.SelectDateWidget(years=range(1900, 2021), empty_label=("年", "月", "日")))
+    age = forms.IntegerField(label='年齢', required=True, min_value=0, max_value=150, widget=forms.Select(choices=choices))
+    # born_at = forms.DateField(label='生年月日', required=True, widget=forms.SelectDateWidget(years=range(1900, 2021), empty_label=("年", "月", "日")))
     introduction = forms.CharField(label='自己紹介', required=False, widget=forms.Textarea(
         attrs={'placeholder': 'はじめまして。太郎です！', 'cols': '30', 'rows': '5'}))
 
