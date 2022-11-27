9f841f1bb18f510ef3a9e5389cf8019e367d258d
fujiya228 <fujiya0xE2@gmail.com>
2022-11-27 19:23:27 +0900

    eslintの設定でエラーが起きていたので修正
    Babel によって変換されないファイル (ツール構成ファイルなど) を lint することができる
    .vueもこれで対応できるっぽい
    https://github.com/babel/babel-eslint
    
    Nuxtとかも初期設定でこうなるっぽい（確かめてはない）

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -8,10 +8,10 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
-    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   }
 }
