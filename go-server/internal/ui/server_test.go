a8e14b185467f3b162812ff091b987e5dd85428c
Hirochon <heacet43@gmail.com>
2023-01-03 19:32:43 +0900

    add: health check test (coverage: 92.9%)

--- go-server/internal/ui/server_test.go
+++ go-server/internal/ui/server_test.go
@@ -1,24 +1,45 @@
 package ui
 
-import "testing"
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Hirochon/Shappar/go-server/internal/ui/gen/models"
+	"github.com/Hirochon/Shappar/go-server/internal/ui/gen/restapiv1/operations/administrations"
+)
 
 func TestServer(t *testing.T) {
 	t.Parallel()
-	cases := []struct {
-		scenario, in, want string
-	}{
-		{
-			scenario: "正常系: Hello World!!が作りたい",
-			in:       "Hello",
-			want:     "Hello World!!",
-		},
+	shapparAPI := NewShapparAPI()
+	shapparServer, err := NewShapparServer(shapparAPI)
+	if err != nil {
+		t.Fatalf("本テストとは関係ないエラーが発生しました: %v", err)
 	}
-	for _, c := range cases {
-		t.Run(c.scenario, func(t *testing.T) {
-			got := Server(c.in)
-			if got != c.want {
-				t.Errorf("want %s, got %s", c.want, got)
-			}
-		})
+	handler := shapparServer.GetHandler()
+	testServer := httptest.NewServer(handler)
+	defer testServer.Close()
+
+	params := administrations.NewGetAPIV1HealthParams()
+	params.HTTPRequest = httptest.NewRequest(http.MethodGet, "http://example.com", nil)
+	resp := healthCheck(params)
+	w := httptest.NewRecorder()
+	resp.WriteResponse(w, shapparAPI.JSONProducer)
+
+	if w.Result().StatusCode != http.StatusOK {
+		t.Errorf("Health Checkのステータスコードが異常です: %v", w.Code)
+	}
+	got, err := io.ReadAll(w.Result().Body)
+	if err != nil {
+		t.Errorf("Health Checkのレスポンスボディの読み込みに失敗しました: %v", err)
+	}
+	unmarshaledGot := models.HealthCheck{}
+	err = unmarshaledGot.UnmarshalJSON(got)
+	if err != nil {
+		t.Errorf("Health CheckのレスポンスボディのUnmarshalに失敗しました: %v", err)
+	}
+	if unmarshaledGot.Status != "OK" {
+		t.Errorf("Health Checkのレスポンスボディが異常です: %v", unmarshaledGot)
 	}
 }
