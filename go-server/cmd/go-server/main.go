dfc2db158509f8c770696d6312a197c79b39bc15
Hirochon <heacet43@gmail.com>
2023-01-03 19:32:17 +0900

    update: server handler

--- go-server/cmd/go-server/main.go
+++ go-server/cmd/go-server/main.go
@@ -1,40 +1,19 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 
 	"github.com/Hirochon/Shappar/go-server/internal/ui"
-	"github.com/Hirochon/Shappar/go-server/internal/ui/gen/restapiv1"
-	"github.com/Hirochon/Shappar/go-server/internal/ui/gen/restapiv1/operations"
-	"github.com/Hirochon/Shappar/go-server/internal/ui/gen/restapiv1/operations/health"
-	"github.com/go-openapi/loads"
-	"github.com/go-openapi/runtime/middleware"
 )
 
-var portFlag = flag.Int("port", 8040, "Port to run this service on")
-
 func main() {
-	fmt.Println(ui.Server("Hello"))
-	swaggerSpec, err := loads.Analyzed(restapiv1.SwaggerJSON, "")
+	shapparServer, err := ui.NewShapparServer(ui.NewShapparAPI())
 	if err != nil {
 		log.Fatalln(err)
 	}
-	api := operations.NewShapparAPI(swaggerSpec)
-	server := restapiv1.NewServer(api)
-	server.EnabledListeners = []string{"http"}
-	defer server.Shutdown()
+	defer shapparServer.Shutdown()
 
-	flag.Parse()
-	server.Port = *portFlag
-
-	api.HealthGetAPIV1HealthHandler = health.GetAPIV1HealthHandlerFunc(
-		func(params health.GetAPIV1HealthParams) middleware.Responder {
-			return health.NewGetAPIV1HealthOK()
-		})
-
-	if err := server.Serve(); err != nil {
+	if err := shapparServer.Serve(); err != nil {
 		log.Fatalln(err)
 	}
 }
--- go-server/internal/ui/server.go
+++ go-server/internal/ui/server.go
@@ -1,7 +1,43 @@
 package ui
 
-import "fmt"
+import (
+	"flag"
 
-func Server(s string) string {
-	return fmt.Sprintf("%s World!!", s)
+	"github.com/Hirochon/Shappar/go-server/internal/ui/gen/models"
+	"github.com/Hirochon/Shappar/go-server/internal/ui/gen/restapiv1"
+	"github.com/Hirochon/Shappar/go-server/internal/ui/gen/restapiv1/operations"
+	"github.com/Hirochon/Shappar/go-server/internal/ui/gen/restapiv1/operations/administrations"
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+)
+
+func healthCheck(params administrations.GetAPIV1HealthParams) middleware.Responder {
+	res := administrations.NewGetAPIV1HealthOK()
+	return res.WithPayload(&models.HealthCheck{
+		Status: "OK",
+	})
+}
+
+func registerHandler(shapparAPI *operations.ShapparAPI) {
+	shapparAPI.AdministrationsGetAPIV1HealthHandler = administrations.GetAPIV1HealthHandlerFunc(healthCheck)
+}
+
+var portFlag = flag.Int("port", 8040, "Port to run this service on")
+
+func NewShapparAPI() *operations.ShapparAPI {
+	swaggerSpec, err := loads.Analyzed(restapiv1.SwaggerJSON, "")
+	if err != nil {
+		panic(err)
+	}
+	shapparAPI := operations.NewShapparAPI(swaggerSpec)
+	registerHandler(shapparAPI)
+	return shapparAPI
+}
+
+func NewShapparServer(shapparAPI *operations.ShapparAPI) (*restapiv1.Server, error) {
+	shapparServer := restapiv1.NewServer(shapparAPI)
+	shapparServer.EnabledListeners = []string{"http"}
+	flag.Parse()
+	shapparServer.Port = *portFlag
+	return shapparServer, nil
 }
